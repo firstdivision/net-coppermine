118027895eba319a16f0d34d3161f5949d602c87
FirstDivision_cp <SND\FirstDivision_cp>
2007-10-12 23:42:08 +0000

    Add resize by longest side

--- DotNetCoppermine/libDotNetCoppermine/ImageResizer.vb
+++ DotNetCoppermine/libDotNetCoppermine/ImageResizer.vb
@@ -8,15 +8,50 @@ Public Class ImageResizer
    ' Code adapted from original article at http://www.glennjones.net/posts/Post.aspx?PostID=799         '
    '----------------------------------------------------------------------------------------------------'
 
+   Public Overloads Sub ResizeToJPEG(ByVal LongestLength As Integer, ByVal OriginalImagePath As String, ByRef OutputStream As Stream, Optional ByVal Quality As Integer = 80)
+      Dim originalimg, thumb As System.Drawing.Image
+      Dim intNewHeight, intNewWidth As Integer
+      Dim sngAspectRatio As Single
+
+      Try
+         originalimg = Image.FromFile(OriginalImagePath) ' Fetch User Filename
+
+         'which side is longest?
+         If originalimg.Height > originalimg.Width Then 'Height is the longest
+            sngAspectRatio = originalimg.Height / LongestLength
+
+            intNewHeight = LongestLength
+            intNewWidth = originalimg.Width / sngAspectRatio
+
+         Else 'Width is the longest
+            sngAspectRatio = originalimg.Width / LongestLength
+
+            intNewWidth = LongestLength
+            intNewHeight = originalimg.Height / sngAspectRatio
+         End If
+
+         'do the actual resize
+         thumb = MakeThumbnail(originalimg, intNewHeight, intNewWidth)
+
+         'output the image
+         OutputImageToStream(thumb, OutputStream, Quality)
+
+         'clean up originalImage (thmb is disposed in "OutputImageToSteeam"
+         originalimg.Dispose()
+      Catch ex As Exception
+         Throw New Exception("Error processing JPEG", ex)
+      End Try
+   End Sub
+
    ''' <summary>
    ''' Resizes a JPEG based on a supplied quality, and a percentage reduction or enlargement.
    ''' </summary>
    ''' <param name="OriginalImagePath">The path to the original JPEG file.</param>
    ''' <param name="OutputStream">The stream that the new image should be resized to (HTTPResponse or FileStream, etc)</param>
    ''' <param name="PercentageToResize">Percentage to resize (.50 for 50 percent)</param>
-   ''' <param name="Quality">Optional quality, 0 to 100, for new JPEG. (Default 100%)</param>
+   ''' <param name="Quality">Optional quality, 0 to 100, for new JPEG. (Default 80%)</param>
    ''' <remarks></remarks>
-   Public Overloads Sub ResizeToJPEG(ByVal OriginalImagePath As String, ByRef OutputStream As Stream, ByVal PercentageToResize As Decimal, Optional ByVal Quality As Integer = 100)
+   Public Overloads Sub ResizeToJPEG(ByVal OriginalImagePath As String, ByRef OutputStream As Stream, ByVal PercentageToResize As Decimal, Optional ByVal Quality As Integer = 80)
 
       If PercentageToResize <= 0 Or PercentageToResize > 1 Then
          Throw New OverflowException("PercentageToResize must be expressed between 0 and 1 (Including 1 but not including zero).")
@@ -49,7 +84,7 @@ Public Class ImageResizer
    ''' <param name="Width">Desired Height for new JPEG.</param>
    ''' <param name="Quality">Optional quality, 0 to 100, for new JPEG. (Default 100%)</param>
    ''' <remarks>If only one of height and width is greater than zero, the other dimension will be scaled proportionally.</remarks>
-   Public Overloads Sub ResizeToJPEG(ByVal OriginalImagePath As String, ByRef OutputStream As Stream, ByVal Height As Integer, ByVal Width As Integer, Optional ByVal Quality As Integer = 100)
+   Public Overloads Sub ResizeToJPEG(ByVal OriginalImagePath As String, ByRef OutputStream As Stream, ByVal Height As Integer, ByVal Width As Integer, Optional ByVal Quality As Integer = 80)
 
       Dim originalimg, thumb As System.Drawing.Image
       Try
@@ -93,7 +128,6 @@ Public Class ImageResizer
          Throw New Exception("Error processing GetEncoder (MimeType)", ex)
       End Try
    End Function
-
 
    Private Function MakeThumbnail(ByVal Source_Img As System.Drawing.Image, ByVal Height As Integer, ByVal Width As Integer) As System.Drawing.Image
       Try
